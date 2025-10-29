924198d9dd283f059cb28693a4608a8d3144c916
Andrew Perotti <Perotti28@gmail.com>
2025-10-29 12:54:29 -0700

    build: add import of router file, use as argument
    This will be used inside RouteProvider as an argument and tells it the routes of our application.

--- main.jsx
+++ main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./src/components/App.jsx";
 import { RouterProvider } from "react-router";
+import router from "./src/assets/router.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider />
+    <RouterProvider router={router} />
   </StrictMode>
 );
