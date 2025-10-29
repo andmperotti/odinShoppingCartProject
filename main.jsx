a6168a04d84169b48f6671bba0c3f31b16834f27
Andrew Perotti <Perotti28@gmail.com>
2025-10-29 12:44:27 -0700

    build: import react-router RouteProvider
    This is used to set up routes in the application.

--- main.jsx
+++ main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./src/components/App.jsx";
+import { RouterProvider } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider />
+  </StrictMode>
 );
