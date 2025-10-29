61e7b9bd2988ad5e4c196c2d2a587146e246c74c
Andrew Perotti <Perotti28@gmail.com>
2025-10-29 13:45:50 -0700

    build: add imports for global styling and routes
    Included in these imports was the StyledNavbar component which was also added to the render method below so an instance is created.

--- main.jsx
+++ main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/index.css";
-import App from "./src/components/App.jsx";
+import "./src/styles/index.css";
 import { RouterProvider } from "react-router";
-import router from "./src/assets/router.js";
+import { router } from "./src/assets/router.js";
+import { StyledNavbar } from "./src/components/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <StyledNavbar />
     <RouterProvider router={router} />
   </StrictMode>
 );
