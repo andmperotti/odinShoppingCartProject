5ba8df01b79ffb1ec51596aae2546d835807731c
Andrew Perotti <Perotti28@gmail.com>
2025-10-29 14:14:30 -0700

    build: add App component
    Although I previously removed this I was thinking at the time where I'd store specific state that needs to persist with the navbar, and thus App has been created again.
    
    build: move StyledNavbar and RouteProvider here
    
    By moving these components into this component and calling this component in main, we can store state in this component and pass it to the StyledNavbar later so that component can display a count of items in the cart in real time.
    
    style: apply a basic style via styled

--- main.jsx
+++ main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./src/styles/index.css";
-import { RouterProvider } from "react-router";
-import { router } from "./src/assets/router.js";
-import { StyledNavbar } from "./src/components/Navbar.jsx";
+import { StyledApp } from "./src/components/App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StyledNavbar />
-    <RouterProvider router={router} />
+    <StyledApp />
   </StrictMode>
 );
