98e7f4b7708af17917fcd280a8a43f72284ad08b
Andrew Perotti <Perotti28@gmail.com>
2025-10-30 12:01:43 -0700

    feat: add ProductCard component
    This component will be used to render a react component for each store item that we'll display to the users
    
    feat: add starter data
    
    The ProductCard component will now return a div with 'Card' and there's now a StyledProductCard which applies a simple purple background to the component.

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+function ProductCard() {
+  return <div>Card</div>;
+}
+
+const StyledProductCard = styled(ProductCard)`
+  background: purple;
+`;
+export { ProductCard, StyledProductCard };
