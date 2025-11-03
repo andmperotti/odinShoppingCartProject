365631a258f57e76af85c6bd72005bc44a94a793
Andrew Perotti <Perotti28@gmail.com>
2025-11-03 11:53:58 -0800

    build: import useState and useEffect
    feat: declare state for products and cart
    
    Products will be populated by an api request to fakestore api via an effect on mount, whereas cart will be populated by users adding items to their cart. Both of these state variables initialize to empty arrays

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,12 @@ import styled from "styled-components";
 import { StyledNavbar } from "./NavBar";
 import { RouterProvider } from "react-router";
 import { router } from "../assets/router";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
+
   return (
     <div>
       <StyledNavbar />
