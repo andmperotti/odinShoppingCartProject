3f45f47e5d04ac0fe4d6da3643e443e6b0a6bcb5
Andrew Perotti <Perotti28@gmail.com>
2025-11-03 08:53:50 -0800

    build: change import of NavBar to StyledNavBar
    We want the styled version therefore we've replaced the original with the styled iteration.
    
    fix: swap NavBar for StyledNavBar
    
    To have a version of NavBar with our styled-components styling applied to it we must call that version of it in the application.

--- src/assets/router.js
+++ src/assets/router.js
@@ -1,11 +1,11 @@
 import { createBrowserRouter } from "react-router";
-import { HomePage } from "../components/HomePage";
+import { StyledHomePage } from "../components/HomePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: HomePage,
-    errorElement: HomePage,
+    Component: StyledHomePage,
+    errorElement: StyledHomePage,
   },
 ]);
 
