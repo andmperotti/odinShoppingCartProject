75169a1c9dc132212f0b451ffa9f5258f162238e
Andrew Perotti <Perotti28@gmail.com>
2025-11-03 08:52:10 -0800

    fix: add className to props and interpolate
    To use a styled-component we must pass in className as a prop to the original component and then apply the className as attribute to it's returned element and interpolate the value of className to that attribute.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
-const Navbar = ({ className }) => <nav className={className}>hi</nav>;
+const Navbar = ({ className }) => <nav className={className}>Nav</nav>;
 
 const StyledNavbar = styled(Navbar)`
   height: fit-content;
   background: gray;
 `;
 export { StyledNavbar, Navbar };
-  
