db2d2b4196bcd97952b3aee5abcbc239476cd6be
Andrew Perotti <Perotti28@gmail.com>
2025-11-03 09:14:38 -0800

    test: add screen.debug
    test: add getByText unit test
    
    This unit test will check if the NavBar component actually renders by searching for the text it was declared to render when called.

--- src/tests/Navbar.test.jsx
+++ src/tests/Navbar.test.jsx
@@ -1,10 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import { Navbar } from "../components/Navbar";
+import { Navbar } from "../components/NavBar";
 import * as React from "react";
 
 describe("Navbar", () => {
-  it("renders Navbar component ", () => {
+  it("renders Navbar component, by looking for content value of 'hi'", () => {
     render(<Navbar />);
+    screen.debug();
+    expect(screen.getByText("Nav")).toBeInTheDocument();
   });
 });
