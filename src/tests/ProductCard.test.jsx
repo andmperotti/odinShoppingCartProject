a2569ac2f3872eecc84522fece7fc0d1f12dfaad
Andrew Perotti <Perotti28@gmail.com>
2025-11-03 09:24:34 -0800

    test: add unit test for text
    This unit test tests whether the component was rendered using the text 'Product'.

--- src/tests/ProductCard.test.jsx
+++ src/tests/ProductCard.test.jsx
@@ -3,10 +3,10 @@ import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import * as React from "react";
 
-describe("ProductCard", ()=>{
-    it("Product Card renders", ()=>{
-        render(<ProductCard />)
-        screen.debug()
-        
-    })
-})
+describe("ProductCard", () => {
+  it("Product Card renders", () => {
+    render(<ProductCard />);
+    screen.debug();
+    expect(screen.getByText("Product")).toBeInTheDocument();
+  });
+});
