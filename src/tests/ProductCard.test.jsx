471c3d06c785d544d987c8c12fa0ffc59e36b573
Andrew Perotti <Perotti28@gmail.com>
2025-11-03 09:22:21 -0800

    test: add imports
    test: define test suite for ProductCard component
    
    test: define test case for the component to render
    
    test: add screen.debug

---
+++ src/tests/ProductCard.test.jsx
@@ -0,0 +1,12 @@
+import { ProductCard } from "../components/ProductCard";
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import * as React from "react";
+
+describe("ProductCard", ()=>{
+    it("Product Card renders", ()=>{
+        render(<ProductCard />)
+        screen.debug()
+        
+    })
+})
