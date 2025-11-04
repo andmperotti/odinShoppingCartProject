7e14821fb5b2fbb0cb1b73cbebbc11c8fcde7283
Andrew Perotti <Perotti28@gmail.com>
2025-11-04 11:53:51 -0800

    feat: add routes for styled cart + shopping pages
    These allow the user to navigate to the pages using the url in their browser instead of the clickable icons atop each page in the NavBar

--- src/assets/router.js
+++ src/assets/router.js
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import { StyledHomePage } from "../components/HomePage";
+import { StyledCartPage } from "../components/CartPage";
+import { StyledShopPage } from "../components/ShopPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: StyledHomePage,
     errorElement: StyledHomePage,
+  },
+  {
+    path: "/shop",
+    Component: StyledShopPage,
+  },
+  {
+    path: "/cart",
+    Component: StyledCartPage,
   },
 ]);
 
