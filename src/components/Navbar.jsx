d0e7762b77d93adfe0494a24515a4bf6239e8e60
Andrew Perotti <Perotti28@gmail.com>
2025-11-04 11:50:53 -0800

    feat: add Link elements wrapping new svg's
    These svg's will become clickable navigation icons for the users to navigate to the other pages of the application.
    
    style: add flex to the component, style svg's
    
    By adding flex and declaring spacing we have a dynamic  layout for our NavBar and I've set a limit on these icons width and height's so they don't get too big.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,39 @@
 import styled from "styled-components";
+import { Link } from "react-router";
 
-const Navbar = ({ className, itemCount }) => (
-  <nav className={className}>Nav CartItems:{itemCount}</nav>
+const Navbar = ({ className }) => (
+  <nav className={className}>
+    <Link to="/">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+        <path d="M304 70.1C313.1 61.9 326.9 61.9 336 70.1L568 278.1C577.9 286.9 578.7 302.1 569.8 312C560.9 321.9 545.8 322.7 535.9 313.8L527.9 306.6L527.9 511.9C527.9 547.2 499.2 575.9 463.9 575.9L175.9 575.9C140.6 575.9 111.9 547.2 111.9 511.9L111.9 306.6L103.9 313.8C94 322.6 78.9 321.8 70 312C61.1 302.2 62 287 71.8 278.1L304 70.1zM320 120.2L160 263.7L160 512C160 520.8 167.2 528 176 528L224 528L224 424C224 384.2 256.2 352 296 352L344 352C383.8 352 416 384.2 416 424L416 528L464 528C472.8 528 480 520.8 480 512L480 263.7L320 120.3zM272 528L368 528L368 424C368 410.7 357.3 400 344 400L296 400C282.7 400 272 410.7 272 424L272 528z" />
+      </svg>
+    </Link>
+
+    <Link to="/shop">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+        <path d="M53.5 245.1L110.3 131.4C121.2 109.7 143.3 96 167.6 96L472.5 96C496.7 96 518.9 109.7 529.7 131.4L586.5 245.1C590.1 252.3 592 260.2 592 268.3C592 295.6 570.8 318 544 319.9L544 512C544 529.7 529.7 544 512 544C494.3 544 480 529.7 480 512L480 320L384 320L384 496C384 522.5 362.5 544 336 544L144 544C117.5 544 96 522.5 96 496L96 319.9C69.2 318 48 295.6 48 268.3C48 260.3 49.9 252.3 53.5 245.1zM160 320L160 432C160 440.8 167.2 448 176 448L304 448C312.8 448 320 440.8 320 432L320 320L160 320z" />
+      </svg>
+    </Link>
+
+    <Link to="/cart">
+      {/* {if itemCount>0} span that displays number of items in cart when there are items, positioned absolutely to this parent top right and maybe just out of it, thereforemaybe adding padding to that this value can sit in the open space */}
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+        <path d="M24 48C10.7 48 0 58.7 0 72C0 85.3 10.7 96 24 96L69.3 96C73.2 96 76.5 98.8 77.2 102.6L129.3 388.9C135.5 423.1 165.3 448 200.1 448L456 448C469.3 448 480 437.3 480 424C480 410.7 469.3 400 456 400L200.1 400C188.5 400 178.6 391.7 176.5 380.3L171.4 352L475 352C505.8 352 532.2 330.1 537.9 299.8L568.9 133.9C572.6 114.2 557.5 96 537.4 96L124.7 96L124.3 94C119.5 67.4 96.3 48 69.2 48L24 48zM208 576C234.5 576 256 554.5 256 528C256 501.5 234.5 480 208 480C181.5 480 160 501.5 160 528C160 554.5 181.5 576 208 576zM432 576C458.5 576 480 554.5 480 528C480 501.5 458.5 480 432 480C405.5 480 384 501.5 384 528C384 554.5 405.5 576 432 576z" />
+      </svg>
+    </Link>
+  </nav>
 );
 
 const StyledNavbar = styled(Navbar)`
   height: fit-content;
   background: gray;
+  display: flex;
+  justify-content: space-around;
+
+  svg {
+    max-width: 25vw;
+    max-height: 100px;
+    width: 100%;
+  }
 `;
 export { StyledNavbar, Navbar };
