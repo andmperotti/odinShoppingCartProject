43616f9eeb747e334555d2d8fa1079b1bf788788
Andrew Perotti <Perotti28@gmail.com>
2025-11-05 12:58:58 -0800

    fix: remove state and useState fetch
    These have been moved to NavBar as it is now the root component with children as it needs to be persistent across the application.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,8 @@
 import styled from "styled-components";
 import { RouterProvider } from "react-router";
 import { router } from "../assets/router";
-import { useState, useEffect } from "react";
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [cartItems, setCartItems] = useState([]);
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((response) => response.json())
-      .then((data) => setProducts([...data]));
-  }, []);
-
   return (
     <div>
       <RouterProvider router={router} />
@@ -20,8 +10,6 @@ function App() {
   );
 }
 
-const StyledApp = styled(App)`
-  background: orange;
-`;
+const StyledApp = styled(App)``;
 
 export { StyledApp, App };
