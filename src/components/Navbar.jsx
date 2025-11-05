51e35504bc66aab4a4c06049fdf3d90da6e514d0
Andrew Perotti <Perotti28@gmail.com>
2025-11-05 12:53:48 -0800

    build: import useEffect, useState, and Outlet
    NavBar is now the primary component of this application, therefore the state will reside in it and the fetch to get products on mount will also reside in it using useEffect.
    
    feat: add headings to navigation icons
    
    This should help users who don't perceive the meaning of the icons to instead be able to read the headings.
    
    style: add styled-components styling
    
    Styling has been added to the many elements to appropriately position dynamically.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,93 @@
 import styled from "styled-components";
 import { Link } from "react-router";
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router";
 
-const Navbar = ({ className }) => (
-  <nav className={className}>
-    <Link to="/">
-      <svg className="feather">
-        <use href="node_modules/feather-icons/dist/feather-sprite.svg#home" />
-      </svg>
-    </Link>
+function Navbar({ className }) {
+  const [products, setProducts] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
-    <Link to="/shop">
-      <svg className="feather">
-        <use href="node_modules/feather-icons/dist/feather-sprite.svg#shopping-bag" />
-      </svg>
-    </Link>
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setProducts([...data]));
+  }, []);
 
-    <Link to="/cart">
-      {/* {if itemCount>0} create a span that displays number of items in cart when there are items, positioned absolutely to this parent top right */}
-      <svg className="feather">
-        <use href="node_modules/feather-icons/dist/feather-sprite.svg#shopping-cart" />
-      </svg>
-    </Link>
-  </nav>
-);
+  return (
+    <div>
+      <nav className={className}>
+        <Link to="/">
+          <h2>Home</h2>
+          <svg className="feather">
+            <use href="node_modules/feather-icons/dist/feather-sprite.svg#home" />
+          </svg>
+        </Link>
+
+        <Link to="/shop">
+          <h2>Shop</h2>
+          <svg className="feather">
+            <use href="node_modules/feather-icons/dist/feather-sprite.svg#shopping-bag" />
+          </svg>
+        </Link>
+
+        <Link to="/cart">
+          <h2>Cart</h2>
+          {cartItems && <span className="cart-count">{cartItems.length}</span>}
+          <svg className="feather cart-feather">
+            <use href="node_modules/feather-icons/dist/feather-sprite.svg#shopping-cart" />
+          </svg>
+        </Link>
+      </nav>
+      <Outlet context={[products, cartItems, setCartItems]} />
+    </div>
+  );
+}
 
 const StyledNavbar = styled(Navbar)`
-  height: fit-content;
   background: gray;
   display: flex;
   justify-content: space-around;
 
+  //svg file
   .feather {
     fill: none;
     stroke: black;
     stroke-width: 1.5;
     stroke-linecap: round;
     stroke-linejoin: round;
+    min-width: 100px;
+    width: 25vw;
+  }
 
-    max-width: 25vw;
-    max-height: 100px;
+  //Link's
+  a {
+    position: relative;
+    z-index: 0;
+    width: min-content;
+  }
+
+  //cart item output
+  .cart-count {
+    color: black;
+    font-size: calc(100% + 0.3vw);
+    background: lightgreen;
+    border-radius: 1vw;
+    padding: 2px 6px;
+    position: absolute;
+    right: 5%;
+    top: 30%;
+  }
+
+  //title over each icon
+  h2 {
+    text-align: center;
+    color: black;
+    text-decoration: none;
+  }
+
+  //taking off text-decoration
+  a {
+    text-decoration: none;
   }
 `;
 export { StyledNavbar, Navbar };
