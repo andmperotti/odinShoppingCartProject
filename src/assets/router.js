0411ea91cc9d7f3ffdf904d90735a07a75729941
Andrew Perotti <Perotti28@gmail.com>
2025-11-05 13:04:48 -0800

    feat: rewrite root to StyledNavBar
    NavBar is now the root component, and such...
    
    feat: add children routes to the root route
    
    In addition to setting up a default child component of StyledHomePage, I've set up the appropriate roots for shop and cart pages. This way the NavBar is persistent and passes the state down to the other components.

--- src/assets/router.js
+++ src/assets/router.js
@@ -2,20 +2,24 @@ import { createBrowserRouter } from "react-router";
 import { StyledHomePage } from "../components/HomePage";
 import { StyledCartPage } from "../components/CartPage";
 import { StyledShopPage } from "../components/ShopPage";
+import { StyledNavbar } from "../components/Navbar";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    Component: StyledHomePage,
-    errorElement: StyledHomePage,
-  },
-  {
-    path: "/shop",
-    Component: StyledShopPage,
-  },
-  {
-    path: "/cart",
-    Component: StyledCartPage,
+    Component: StyledNavbar,
+    errorElement: StyledNavbar,
+    children: [
+      { index: true, Component: StyledHomePage },
+      {
+        path: "/shop",
+        Component: StyledShopPage,
+      },
+      {
+        path: "/cart",
+        Component: StyledCartPage,
+      },
+    ],
   },
 ]);
 
