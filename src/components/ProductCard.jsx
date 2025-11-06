8bf1af1aa041efcc85660b33f9533f8d74797d23
Andrew Perotti <Perotti28@gmail.com>
2025-11-06 12:43:24 -0800

    build: import useOutletContext
    This will be used to pull in state from NavBar.
    
    feat: add data to component for products
    
    This entails a header with the product title, and image of the product provided by the api, the rating of the product by users from the api, a product description, product price, and finally an input field and button to add a quantity of the product to the shopping cart of the user.
    
    style: add styling to elements

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,40 @@
 import styled from "styled-components";
+import { useOutletContext } from "react-router";
 
-function ProductCard() {
-  return <div>Product</div>;
+function ProductCard({ className, product }) {
+  return (
+    <div className={className}>
+      <h2>{product.title}</h2>
+      <img src={product.image} alt={product.title} />
+      <span>
+        Rating: {product.rating.rate} ({product.rating.count} ratings)
+      </span>
+      <p>{product.description}</p>
+      <span className="product-price">{product.price.toFixed(2)}</span>
+      <div>
+        {/* quantity input field */}
+        <input type="number" class="order-quantity-input"></input>
+        {/* add to cart button */}
+        <button type="button">Add to cart</button>
+      </div>
+    </div>
+  );
 }
 
 const StyledProductCard = styled(ProductCard)`
-  background: purple;
+  border: 1px solid lightgray;
+  border-radius: 5px;
+  padding: 1ch;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .product-price {
+    font-weight: bold;
+  }
+  .order-quantity-input {
+    width: 10ch;
+  }
 `;
 export { ProductCard, StyledProductCard };
