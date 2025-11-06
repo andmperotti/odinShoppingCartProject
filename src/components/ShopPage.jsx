e53692ebe974bd972a48501c2307b3a9d48154c1
Andrew Perotti <Perotti28@gmail.com>
2025-11-06 12:10:03 -0800

    build: import useOutletContext
    This allows us to destructure props from the parent component into this component, here being the products from the fakestore api.
    
    build: import StyledProductCard
    
    This child component will build components for each product in the fakestore api offering, which will give users info on th products and the ability to add them to their shopping carts.
    
    style: centered h1

--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -1,10 +1,27 @@
 import styled from "styled-components";
+import { useOutletContext } from "react-router";
+import { StyledProductCard } from "./ProductCard";
 
-function ShopPage() {
-  return <div>Shop</div>;
+function ShopPage({ className }) {
+  const [products] = useOutletContext();
+  return (
+    <div className={className}>
+      <h1>Shop</h1>
+      <ul>
+        {products.map((product, index) => (
+          <StyledProductCard product={product} key={index} />
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 const StyledShopPage = styled(ShopPage)`
-  background: lightgreen;
+  ul > * {
+    margin: 1ch 0;
+  }
+  h1 {
+    text-align: center;
+  }
 `;
 export { ShopPage, StyledShopPage };
