b6d9418f90ab36e8c571527d19de74d2204fd1af
Andrew Perotti <Perotti28@gmail.com>
2025-11-06 11:23:54 -0800

    build: import useOutletContext
    This component is now a child of NavBar and for its parent to pass down state we'll need to use context attribute in Outlet in the parent and  import it into the component by destructuring with useOutletContext.
    
    feat: destructure state from context of Outlet
    
    This state variable and its state setting function will allow us to display items in a users shopping card, and to edit them in quantity via the state's setter function.

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,7 +1,16 @@
 import styled from "styled-components";
+import { Link } from "react-router";
+import { StyledNavbar } from "./Navbar";
+import { useOutletContext } from "react-router";
 
-function CartPage() {
-  return <div>Cart</div>;
+//non prop version other than styled-content className prop
+function CartPage({ className }) {
+  const [cartItems, setCartItems] = useOutletContext();
+  return (
+    <div className={className}>
+      <h2>Shopping Cart</h2>
+    </div>
+  );
 }
 
 const StyledCartPage = styled(CartPage)`
