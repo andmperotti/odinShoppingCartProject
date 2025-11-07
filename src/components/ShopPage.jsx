41b0aa300dc237c6bbfe3962b149282ce17f0daa
Andrew Perotti <Perotti28@gmail.com>
2025-11-07 13:54:39 -0800

    feat: destructure cartItems and setCartItems
    In addition to the original products variable being destructured from the Outlet context, we need to destructure the shopping cart state so we can pass it down to the StyledProductCard...
    
    feat: add cartItems and setCartItems as props
    
    To the StyledProductCard, these will be used to generate elements for each of the products and logic to add those products to a shopping cart.

--- src/components/ShopPage.jsx
+++ src/components/ShopPage.jsx
@@ -3,13 +3,18 @@ import { useOutletContext } from "react-router";
 import { StyledProductCard } from "./ProductCard";
 
 function ShopPage({ className }) {
-  const [products] = useOutletContext();
+  const [products, cartItems, setCartItems] = useOutletContext();
   return (
     <div className={className}>
       <h1>Shop</h1>
       <ul>
         {products.map((product, index) => (
-          <StyledProductCard product={product} key={index} />
+          <StyledProductCard
+            product={product}
+            key={index}
+            cartItems={cartItems}
+            setCartItems={setCartItems}
+          />
         ))}
       </ul>
     </div>
