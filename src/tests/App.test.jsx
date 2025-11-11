33822c2c732cc115d030abee6daebe85e81a0de7
Andrew Perotti <Perotti28@gmail.com>
2025-11-11 11:31:33 -0800

    test: change the text to look for
    Since the last time I rand tests, this pages content has changed but now always has a heading of 'Random Store', so we tell our test to look for that string in the document. I also added some more words to the test case description.

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -4,9 +4,9 @@ import * as React from "react";
 import { App } from "../components/App";
 
 describe("App component", () => {
-  it("App renders NavBar which contains 'Nav'", () => {
+  it("App renders NavBar which by default renders the home page that contains 'Random Store'", () => {
     render(<App />);
     screen.debug();
-    expect(screen.getByText("Nav")).toBeInTheDocument();
+    expect(screen.getByText("Random Store")).toBeInTheDocument();
   });
 });
