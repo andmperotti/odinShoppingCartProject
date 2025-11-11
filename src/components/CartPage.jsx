918dbf036a684ab4bcbeb7b042d40f4c80440c2e
Andrew Perotti <Perotti28@gmail.com>
2025-11-11 09:26:11 -0800

    feat: add totalCartPrice variable
    This variable's value will be interpolated below all CartCard/product elements to display the total cost of the shopping cart contents to the user.
    
    feat: add checkout button
    
    This is just for looks and does not actually do anything.
    
    feat: add dynamic content
    
    I added an if else conditional statement that checks the number of items in the cartItems state and if there are any items then returns a header and a CartCard component for each product in cartItems and uses the totalCartPrice to communicate their shopping cart total price, otherwise returns a paragraph element that tells the user their cart is empty.

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -1,17 +1,59 @@
 import styled from "styled-components";
 import { useOutletContext } from "react-router";
+import { StyledCartCard } from "./CartCard";
 
 //non prop version other than styled-content className prop
 function CartPage({ className }) {
-  const [cartItems, setCartItems] = useOutletContext();
-  return (
-    <div className={className}>
-      <h2>Shopping Cart</h2>
-    </div>
+  const [products, cartItems, setCartItems] = useOutletContext();
+  let totalCartPrice = cartItems.reduce(
+    (total, eachProduct) =>
+      total + products[eachProduct.id - 1].price * eachProduct.quantity,
+    0
   );
+
+  if (cartItems.length > 0) {
+    return (
+      <div className={className}>
+        <h2>Shopping Cart</h2>
+        <ul>
+          {cartItems.map((cartItem) => (
+            <StyledCartCard
+              cartItem={cartItem}
+              key={cartItem.id}
+              products={products}
+              setCartItems={setCartItems}
+              cartItems={cartItems}
+            />
+          ))}
+        </ul>
+        <div className="total-cost">
+          <p>Total Cart Price: {totalCartPrice.toFixed(2)}</p>
+          <button type="button">Checkout</button>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={className}>
+        <h2>Shopping Cart</h2>
+        <p>Nothing in cart</p>
+      </div>
+    );
+  }
 }
 
+// value={cartItems[product.id - 1].quantity}
+
 const StyledCartPage = styled(CartPage)`
-  background: lightgreen;
+  h2,
+  p {
+    text-align: center;
+  }
+  .total-cost {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin-right: 3vw;
+  }
 `;
 export { CartPage, StyledCartPage };
