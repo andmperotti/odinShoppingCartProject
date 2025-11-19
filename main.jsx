5c985920cc0dadd88f9bd5c08b8504eb49b74b4a
Andrew Perotti <Perotti28@gmail.com>
2025-11-19 09:02:21 -0800

    fix: move RouterProvider to main.jsx
    Before this change RouterProvider was in App, and App would pretty much create instances of NavBar, but this was incorrect. So now App is the root component and it calls an instance of NavBar and then the appropriate component below it depending on what is entered in the URL.

--- main.jsx
+++ main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./src/styles/index.css";
 import { StyledApp } from "./src/components/App.jsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./src/assets/router.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <StyledApp />
+    <RouterProvider router={router} />
   </StrictMode>
 );
