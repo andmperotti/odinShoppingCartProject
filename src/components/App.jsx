3af7bc9dc4723328d1c4b83bafa28c242ec108a3
Andrew Perotti <Perotti28@gmail.com>
2025-11-20 10:30:51 -0800

    fix: change react-router import
    Turns out Outlet needs to come from react-router-dom if I want to test the children of App, this took more time than I'd like to admit.
    
    test: add data-testid attribute to return element
    
    This attribute will help me to target the expected out put of this Component in tests.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { NavBar, StyledNavBar } from "./NavBar";
 import { useEffect, useState } from "react";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -16,7 +16,7 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div data-testid="app">
       <StyledNavBar cartItems={cartItems} />
       <Outlet context={[products, cartItems, setCartItems]} />
     </div>
