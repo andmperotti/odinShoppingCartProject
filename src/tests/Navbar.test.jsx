939ccd1557286be964a8d5205bc97c6ca2bb2b9c
Andrew Perotti <Perotti28@gmail.com>
2025-11-21 12:28:13 -0800

    test: add render test for NavBar component
    This test checks whether the NavBar component is rendered when users navigate to the root route.

--- src/tests/Navbar.test.jsx
+++ src/tests/Navbar.test.jsx
@@ -1,12 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
-import { Navbar } from "../components/NavBar";
 import * as React from "react";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { routes } from "../assets/router";
+import { StyledHomePage } from "../components/HomePage";
+import { StyledNavBar } from "../components/NavBar";
+import { StyledShopPage } from "../components/ShopPage";
 
-describe("Navbar", () => {
-  it("renders Navbar component, by looking for content value of 'hi'", () => {
-    render(<Navbar />);
-    screen.debug();
-    expect(screen.getByText("Nav")).toBeInTheDocument();
+describe("NavBar Component", () => {
+  it("renders NavBar by checking for the testid present", () => {
+    const memoryRouter = createMemoryRouter(routes);
+
+    render(<RouterProvider router={memoryRouter} />);
+
+    expect(screen.getByTestId("NavBar")).toBeInTheDocument();
   });
 });
