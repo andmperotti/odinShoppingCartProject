97ee5e95c69676e381cc49db130995751b51d061
Andrew Perotti <Perotti28@gmail.com>
2025-11-21 12:26:27 -0800

    test: add test for HomePage render
    This test checks if HomePage is rendered when users navigate to the root route.

--- src/tests/Homepage.test.jsx
+++ src/tests/Homepage.test.jsx
@@ -1,12 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import * as React from "react";
-import { HomePage } from "../components/HomePage";
+import { RouterProvider, createMemoryRouter } from "react-router-dom";
+import { routes } from "../assets/router";
+import { StyledHomePage } from "../components/HomePage";
+import { StyledNavBar } from "../components/NavBar";
+import { StyledShopPage } from "../components/ShopPage";
 
-describe("HomePage component", () => {
-  it("HomePage renders", () => {
-    render(<HomePage />);
-    screen.debug();
-    expect(screen.getByText("Home")).toBeInTheDocument();
+describe("NavBar Component", () => {
+  it("renders NavBar by checking for the testid present", () => {
+    const memoryRouter = createMemoryRouter(routes);
+
+    render(<RouterProvider router={memoryRouter} />);
+
+    expect(screen.getByTestId("HomePage")).toBeInTheDocument();
   });
 });
