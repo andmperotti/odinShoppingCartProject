6a815636742afcc79a246013eb317bbb28fde7c4
Andrew Perotti <Perotti28@gmail.com>
2025-11-21 12:39:55 -0800

    test: add mock cleanup

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -1,9 +1,12 @@
-import { expect, afterEach } from 'vitest';
-import { cleanup } from '@testing-library/react';
+import { expect, afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
 import * as matchers from "@testing-library/jest-dom/matchers";
+import "@testing-library/jest-dom/vitest";
+import { vi } from "vitest";
 
 expect.extend(matchers);
 
 afterEach(() => {
   cleanup();
+  vi.clearAllMocks();
 });
