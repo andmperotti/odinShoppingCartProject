7ace7bc42ce45c622d9dffb08295eacb18dc73ac
Andrew Perotti <Perotti28@gmail.com>
2025-11-21 12:25:04 -0800

    test: add test to check if CartPage is rendered
    This specifically checks for if the component is rendered when a user navigates to the route '/cart'.

--- src/tests/CartPage.test.jsx
+++ src/tests/CartPage.test.jsx
@@ -5,8 +5,10 @@ import { CartPage } from "../components/CartPage.jsx";
 
 describe("CartPage component", () => {
   it("CartPage renders, by testing for 'Shopping Cart' to be in document ", () => {
-    render(<CartPage />);
+    render(<CartPage cartItems={[{ id: 1, quantity: 1 }]} />);
     screen.debug();
     expect(screen.getByText("Shopping Cart")).toBeInTheDocument();
   });
 });
+
+//mock cartItems, and check if a CartCard will be rendered when items in cart, and check if 'Nothing in cart' is rendered when the cartItems is empty/bad fetch.
