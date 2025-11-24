92b29f441b469f8e31dc655a8b7ca2cc11eab273
Andrew Perotti <Perotti28@gmail.com>
2025-11-24 09:25:02 -0800

    test: change initial test to use RouteProvider
    This test also uses getByTestId instead of checking for text rendered in the component.
    
    test: add imports
    
    To use RouteProvider we not only needed to import it but also our routes object for the project and createMemoryRouter to create a router that our test file can use.

--- src/tests/CartPage.test.jsx
+++ src/tests/CartPage.test.jsx
@@ -2,13 +2,23 @@ import { render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import * as React from "react";
 import { CartPage } from "../components/CartPage.jsx";
+import { createMemoryRouter } from "react-router-dom";
+import { routes } from "../assets/router.js";
+import { RouterProvider } from "react-router-dom";
 
 describe("CartPage component", () => {
   it("CartPage renders, by testing for 'Shopping Cart' to be in document ", () => {
-    render(<CartPage cartItems={[{ id: 1, quantity: 1 }]} />);
+    const memoryRouter = createMemoryRouter(routes, {
+      initialEntries: ["/cart"],
+    });
+    render(<RouterProvider router={memoryRouter} />);
     screen.debug();
-    expect(screen.getByText("Shopping Cart")).toBeInTheDocument();
+    expect(screen.getByTestId("CartPage")).toBeInTheDocument();
   });
+
+  // it("CartPage communicates to users that they have no products in their cart", () => {
+  //   render(<CartPage cartItems={[{ id: 1, quantity: 1 }]} />);
+  // });
 });
 
 //mock cartItems, and check if a CartCard will be rendered when items in cart, and check if 'Nothing in cart' is rendered when the cartItems is empty/bad fetch.
