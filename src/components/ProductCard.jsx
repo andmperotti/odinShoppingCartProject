a73b5b2a0715e9ad9bbebdec9aa5b03968f8efdf
Andrew Perotti <Perotti28@gmail.com>
2025-11-24 12:42:49 -0800

    fix: change span to label
    This label now wraps the input element of ProductCard instances which allows the user to enter the input element when clicking the label, and gives semantic value this element as well.
    
    fix: change onClick functionality
    
    The changes to this action now show an error message when a user has not entered a quantity of an item to add to their carts but have clicked on the 'Add to Cart' button. Before these changes there was no message and there was code executing to change nothing in the background, which won't invoke now until there is an input value greater than zero. This error message also is not spammable which was prevented by adding a variable to the component scope and using a condition off that value before showing that error message.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 function ProductCard({ className, product, cartItems, setCartItems }) {
+  let errorShown = false;
+
   return (
     <div className={className} data-testid="ProductCard">
       <h2>{product.title}</h2>
@@ -14,24 +16,42 @@ function ProductCard({ className, product, cartItems, setCartItems }) {
         Price each: ${product.price.toFixed(2)}
       </span>
       <div>
-        <span>Quantity: </span>
-        <input type="number" className="order-quantity-input" min="1"></input>
+        <label>
+          Quantity:
+          <input type="number" className="order-quantity-input" min="1"></input>
+        </label>
         <button
           type="button"
           onClick={(e) => {
-            let newCartItems = [...cartItems];
-            let inputElement = e.target.parentNode.children[1];
-            if (
-              newCartItems.filter((item) => item.id === product.id).length === 0
-            ) {
-              newCartItems.push({ id: product.id, quantity: 0 });
-              newCartItems.sort((a, b) => a.id - b.id);
+            let inputFieldValue = document.querySelector("input").value;
+            if (inputFieldValue > 0) {
+              let newCartItems = [...cartItems];
+              let inputElement = e.target.parentNode.children[0].children[0];
+              if (
+                newCartItems.filter((item) => item.id === product.id).length ===
+                0
+              ) {
+                newCartItems.push({ id: product.id, quantity: 0 });
+                newCartItems.sort((a, b) => a.id - b.id);
+              }
+              let currentItemIndex = newCartItems.findIndex(
+                (item) => item.id === product.id
+              );
+              newCartItems[currentItemIndex].quantity += +inputElement.value;
+              setCartItems(newCartItems);
+            } else {
+              if (errorShown === false) {
+                errorShown = true;
+                let errorMessage = document.createElement("span");
+                errorMessage.classList.add("error-message");
+                errorMessage.innerText = "Must enter a quantity greater than 0";
+                e.target.parentElement.after(errorMessage);
+                setTimeout(() => {
+                  document.querySelector(".error-message").remove();
+                  errorShown = false;
+                }, 3000);
+              }
             }
-            let currentItemIndex = newCartItems.findIndex(
-              (item) => item.id === product.id
-            );
-            newCartItems[currentItemIndex].quantity += +inputElement.value;
-            setCartItems(newCartItems);
           }}
         >
           Add to cart
@@ -68,6 +88,9 @@ const StyledProductCard = styled(ProductCard)`
   }
   p {
     text-align: center;
+  }
+  .error-message {
+    color: red;
   }
 `;
 export { ProductCard, StyledProductCard };
